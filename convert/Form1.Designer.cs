3925d354343db1674c47600dd4a41c00b1b3177b
VanLuu <vanluu.g@gmail.com>
2013-12-16 17:50:25 +0700

    + add source

---
+++ convert/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace convert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.tabPage7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Location = new System.Drawing.Point(13, 40);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1248, 791);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1240, 782);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1240, 782);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1240, 782);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.richTextBox4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1240, 782);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.richTextBox5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1240, 782);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.richTextBox6);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1240, 765);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "tabPage6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox3.TabIndex = 1;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox4.TabIndex = 1;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox5.TabIndex = 1;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox6.TabIndex = 1;
+            this.richTextBox6.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.richTextBox7);
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(1240, 765);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "tabPage7";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(7, -2);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(1227, 769);
+            this.richTextBox7.TabIndex = 2;
+            this.richTextBox7.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 843);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage7.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+    }
+}
+
---
+++ convert/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace convert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox7.Text = File.ReadAllText(openFileDialog1.FileName, Encoding.UTF8);
+
+                List<string> lLineEN = new List<string>();
+                List<string> lLineVI = new List<string>();
+                List<string> lLineDE = new List<string>();
+                List<string> lLineID = new List<string>();
+                List<string> lLineTH = new List<string>();
+                List<string> lLineRU = new List<string>();
+                foreach (string line in richTextBox7.Lines)
+                {
+                    if (line.Trim() != "")
+                    {
+                        string[] temp = line.Split('\t');
+                        lLineEN.Add(temp[0].Replace("\"", "\\\"").Replace("  ", " ").Trim());
+                        lLineVI.Add(temp[1].Replace("\"", "\\\"").Replace("  ", " ").Trim());
+                        lLineDE.Add(temp[2].Replace("\"", "\\\"").Replace("  ", " ").Trim());
+                        lLineID.Add(temp[3].Replace("\"", "\\\"").Replace("  ", " ").Trim());
+                        lLineTH.Add(temp[4].Replace("\"", "\\\"").Replace("  ", " ").Trim());
+                        lLineRU.Add(temp[5].Replace("\"", "\\\"").Replace("  ", " ").Trim());
+                    }
+                }
+                richTextBox7.Text = "";
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox7.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox7.AppendText("\n");
+                }
+                //***-EN
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox1.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("\"" + lLineVI[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("\"" + lLineDE[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("\"" + lLineID[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("\"" + lLineTH[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("\"" + lLineRU[i] + "\"=\"" + lLineEN[i] + "\";");
+                    richTextBox1.AppendText("\n");
+                }
+
+                //***-VI
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox2.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineVI[i] + "\";");
+                    richTextBox2.AppendText("\n");
+                    richTextBox2.AppendText("\"" + lLineVI[i] + "\"=\"" + lLineVI[i] + "\";");
+                    richTextBox2.AppendText("\n");
+                    richTextBox2.AppendText("\"" + lLineDE[i] + "\"=\"" + lLineVI[i] + "\";");
+                    richTextBox2.AppendText("\n");
+                    richTextBox2.AppendText("\"" + lLineID[i] + "\"=\"" + lLineVI[i] + "\";");
+                    richTextBox2.AppendText("\n");
+                    richTextBox2.AppendText("\"" + lLineTH[i] + "\"=\"" + lLineVI[i] + "\";");
+                    richTextBox2.AppendText("\n");
+                    richTextBox2.AppendText("\"" + lLineRU[i] + "\"=\"" + lLineVI[i] + "\";");
+                    richTextBox2.AppendText("\n");
+                }
+
+                //***-DE
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox3.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineDE[i] + "\";");
+                    richTextBox3.AppendText("\n");
+                    richTextBox3.AppendText("\"" + lLineVI[i] + "\"=\"" + lLineDE[i] + "\";");
+                    richTextBox3.AppendText("\n");
+                    richTextBox3.AppendText("\"" + lLineDE[i] + "\"=\"" + lLineDE[i] + "\";");
+                    richTextBox3.AppendText("\n");
+                    richTextBox3.AppendText("\"" + lLineID[i] + "\"=\"" + lLineDE[i] + "\";");
+                    richTextBox3.AppendText("\n");
+                    richTextBox3.AppendText("\"" + lLineTH[i] + "\"=\"" + lLineDE[i] + "\";");
+                    richTextBox3.AppendText("\n");
+                    richTextBox3.AppendText("\"" + lLineRU[i] + "\"=\"" + lLineDE[i] + "\";");
+                    richTextBox3.AppendText("\n");
+                }
+
+                //***-ID
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox4.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineID[i] + "\";");
+                    richTextBox4.AppendText("\n");
+                    richTextBox4.AppendText("\"" + lLineVI[i] + "\"=\"" + lLineID[i] + "\";");
+                    richTextBox4.AppendText("\n");
+                    richTextBox4.AppendText("\"" + lLineDE[i] + "\"=\"" + lLineID[i] + "\";");
+                    richTextBox4.AppendText("\n");
+                    richTextBox4.AppendText("\"" + lLineID[i] + "\"=\"" + lLineID[i] + "\";");
+                    richTextBox4.AppendText("\n");
+                    richTextBox4.AppendText("\"" + lLineTH[i] + "\"=\"" + lLineID[i] + "\";");
+                    richTextBox4.AppendText("\n");
+                    richTextBox4.AppendText("\"" + lLineRU[i] + "\"=\"" + lLineID[i] + "\";");
+                    richTextBox4.AppendText("\n");
+                }
+
+                //***-TH
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox5.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineTH[i] + "\";");
+                    richTextBox5.AppendText("\n");
+                    richTextBox5.AppendText("\"" + lLineVI[i] + "\"=\"" + lLineTH[i] + "\";");
+                    richTextBox5.AppendText("\n");
+                    richTextBox5.AppendText("\"" + lLineDE[i] + "\"=\"" + lLineTH[i] + "\";");
+                    richTextBox5.AppendText("\n");
+                    richTextBox5.AppendText("\"" + lLineID[i] + "\"=\"" + lLineTH[i] + "\";");
+                    richTextBox5.AppendText("\n");
+                    richTextBox5.AppendText("\"" + lLineTH[i] + "\"=\"" + lLineTH[i] + "\";");
+                    richTextBox5.AppendText("\n");
+                    richTextBox5.AppendText("\"" + lLineRU[i] + "\"=\"" + lLineTH[i] + "\";");
+                    richTextBox5.AppendText("\n");
+                }
+
+                //***-RU
+                for (int i = 0; i < lLineEN.Count; i++)
+                {
+                    richTextBox6.AppendText("\"" + lLineEN[i] + "\"=\"" + lLineRU[i] + "\";");
+                    richTextBox6.AppendText("\n");
+                    richTextBox6.AppendText("\"" + lLineVI[i] + "\"=\"" + lLineRU[i] + "\";");
+                    richTextBox6.AppendText("\n");
+                    richTextBox6.AppendText("\"" + lLineDE[i] + "\"=\"" + lLineRU[i] + "\";");
+                    richTextBox6.AppendText("\n");
+                    richTextBox6.AppendText("\"" + lLineID[i] + "\"=\"" + lLineRU[i] + "\";");
+                    richTextBox6.AppendText("\n");
+                    richTextBox6.AppendText("\"" + lLineTH[i] + "\"=\"" + lLineRU[i] + "\";");
+                    richTextBox6.AppendText("\n");
+                    richTextBox6.AppendText("\"" + lLineRU[i] + "\"=\"" + lLineRU[i] + "\";");
+                    richTextBox6.AppendText("\n");
+                }
+            }
+        }
+    }
+}
